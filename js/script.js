8e74f427cdfd6e9e8a28e7b45d75db16a1eeb52f
lukas <luki.minda@gmail.com>
2025-02-17 19:30:54 +0100

    Remove submodule reference for Frontend-quiz

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const timer = {
+    pomodoro: 25,
+    shortbreak: 5,
+    longbreak: 15,
+    longbreakinterval: 4
+
+
+}
+
+
+
+
+let btns = document.querySelectorAll("a");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        btns.forEach(btn => btn.classList.remove("active"));
+        this.classList.add("active");
+    });
+}
